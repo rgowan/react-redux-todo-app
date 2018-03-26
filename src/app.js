6ed3bd94743c41c5c6ad3300508aa2eb9bf1f889
rgowan <ranegowan@gmail.com>
2018-03-26 14:31:19 +0100

    adding basic components

--- src/app.js
+++ src/app.js
@@ -1,15 +1,53 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+
+import TodoList from './components/TodoList';
+
+const todos = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [...state, { id: action.id, text: action.text, completed: false }];
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id !== action.id)
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if(todo.id !== action.id) return todo;
+        todo.completed = !todo.completed;
+        return todo;
+      });
+  }
+}
+
+// const visibilityFilter = (state = 'SHOW_ALL', action) => {
+//   switch(action.type) {
+//     case 'SET_VISIBILITY_FILTER':
+//       return action.visibilityFilter
+//   }
+// }
+
+const store = createStore(todos);
 
 class App extends React.Component {
   render() {
     return(
-      <h1>Hello World</h1>
+      <main>
+        <h1>React Redux Todo App</h1>
+        <TodoList />
+      </main>
     );
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
+
+
+
---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = ({
+
+}) => (
+  <li>Hello World</li>
+);
+
+export default Todo;
